b9cfdcf94117421135cb0ac9b723a2a783314f0c
Jon Mifsud <info@jonmifsud.com>
2013-05-27 09:09:47 +0200

    push proper extension driver & js update

--- assets/author_preselect.js
+++ assets/author_preselect.js
@@ -8,7 +8,7 @@
 	$(function() {		
 		$authorField = $('select[name="fields[author]"]');
 
-		if ($authorField.length()>0 && $authorField.find('option:selected').val()=""){
+		if ($authorField.length>0 && $authorField.find('option:selected').val()==""){
 			$authorField.find('option:contains('+Symphony.Author.first_name+' '+Symphony.Author.last_name+')').attr('selected', 'selected');
 		}
 	});
--- extension.driver.php
+++ extension.driver.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Backend language switcher
+ * Author PreSelect
  *
- * @author John J. Camilleri
- * @version 1.2.1
+ * @author Jon Mifsud
+ * @version 0.1
  */
-Class extension_backend_language_switcher extends Extension{
+Class extension_author_preselect extends Extension{
 	
 	public function getSubscribedDelegates(){
 		return array(
@@ -17,49 +17,12 @@ Class extension_backend_language_switcher extends Extension{
 		);
 	}
 	
-	private $LOAD_NUMBER = 955935299;
+	private $LOAD_NUMBER = 955968299;
 
 	public function initializeAdmin($context) {
-		$assets_path = URL . '/extensions/backend_language_switcher/assets';
 		$page = Administration::instance()->Page;
-		$author = Administration::instance()->Author;
-		
-		//frontend localization
-		$codes = Symphony::Configuration()->get('langs', 'frontend_localisation');
-		//language redirect cases
-		if ($codes == '' || $codes == null) $codes = Symphony::Configuration()->get('language_codes', 'language_redirect');
-		if ($codes == '' || $codes == null) $codes = Symphony::Configuration()->get('languages', 'language_redirect');
-		$languages = array_map('trim',explode(',', $codes ));
-		
-		// CSS & JS for all admin
-		$page->addStylesheetToHead($assets_path . '/language_switcher.css', 'all', $LOAD_NUMBER++);
-		$script = new XMLElement('script');
-		$script->setAttributeArray(array('type' => 'text/javascript'));
-		$script->setValue(sprintf(
-			"
-				Symphony.Languages = ['%s'];
-				Symphony.Author = {
-					id : %d,
-					default_section : %d,
-					email : '%s',
-					first_name : '%s',
-					last_name : '%s',
-					username : '%s',
-					language : '%s'
-				};
-			",
-			implode("','", $languages),
-			$author->get('id'),
-			$author->get('default_section'),
-			$author->get('email'),
-			addslashes($author->get('first_name')),
-			addslashes($author->get('last_name')),
-			$author->get('username'),
-			$author->get('language')
-		));
-		$script->setSelfClosingTag(false);
-		$page->addElementToHead($script, $this->LOAD_NUMBER++);
-		$page->addScriptToHead($assets_path . '/language_switcher.js', $this->LOAD_NUMBER++);
+		$assets_path = URL . '/extensions/author_preselect/assets';
+		$page->addScriptToHead($assets_path . '/author_preselect.js', $this->LOAD_NUMBER++);
 	}
 	
 	public function enable() {
